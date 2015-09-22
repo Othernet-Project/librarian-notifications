b3e3d6056e3191fe3624752e62de0601cdb676ac
Andrean Franc <andrean.franc@gmail.com>
2015-09-22 12:03:28 +0200

    Register notification menu item explicitly

--- librarian_notifications/hooks.py
+++ librarian_notifications/hooks.py
@@ -1,5 +1,7 @@
 from .notifications import Notification
+from .menuitems import NotificationMenuItem
 
 
 def initialize(supervisor):
     supervisor.exts.notifications = Notification
+    supervisor.exts.menuitems.register(NotificationMenuItem)
